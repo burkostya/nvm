5164bf641593d45700bcc7083297859336601758
Konstantin Burykin <burkostya@gmail.com>
2012-11-22 07:44:23 +0000

    first attemp to fishify nvm

--- install.sh
+++ install.sh
@@ -1,51 +1,48 @@
-#!/bin/bash
+set NVM_TARGET "$HOME/.nvm"
 
-NVM_TARGET="$HOME/.nvm"
-
-if [ -d "$NVM_TARGET" ]; then
+if test -d $NVM_TARGET
   echo "=> NVM is already installed in $NVM_TARGET, trying to update"
-  echo -ne "\r=> "
-  cd $NVM_TARGET && git pull
+  echo -n "=> "
+  cd $NVM_TARGET; and git pull
   exit
-fi
+end
 
 # Cloning to $NVM_TARGET
-git clone git://github.com/creationix/nvm.git $NVM_TARGET
+git clone git://github.com/burkostya/nvm.git $NVM_TARGET
 
 echo 
 
-# Detect profile file, .bash_profile has precedence over .profile
-if [ ! -z "$1" ]; then
-  PROFILE="$1"
+# Detect profile file, .config/fish/config.fish has precedence over etc/fish/config.fish
+if not test -z $argv[1]
+  set PROFILE $argv[1]
 else
-  if [ -f "$HOME/.bash_profile" ]; then
-	PROFILE="$HOME/.bash_profile"
-  elif [ -f "$HOME/.profile" ]; then
-	PROFILE="$HOME/.profile"
-  fi
-fi
 
-SOURCE_STR='[[ -s "$HOME/.nvm/nvm.sh" ]] && . "$HOME/.nvm/nvm.sh"  # This loads NVM'
+  if test -f "$HOME/.config/fish/config.fish"
+	  set PROFILE "$HOME/.config/fish/config.fish"
+  else if test -f "$HOME/etc/fish/config.fish"
+	  set PROFILE "$HOME/etc/fish/config.fish"
+  end
+end
 
-if [ -z "$PROFILE" ] || [ ! -f "$PROFILE" ] ; then
-  if [ -z $PROFILE ]; then 
-	echo "=> Profile not found"
-  else
-	echo "=> Profile $PROFILE not found"
-  fi
+set SOURCE_STR 'test -s "$HOME/.nvm/nvm.sh"; and . "$HOME/.nvm/nvm.sh"  # This loads NVM'
+
+if test -z $PROFILE 
+  echo "=> Profile $PROFILE not found"
+  exit
+else if not test -f $PROFILE
   echo "=> Append the following line to the correct file yourself"
   echo
-  echo -e "\t$SOURCE_STR"
+  echo \t"$SOURCE_STR"
   echo  
   echo "=> Close and reopen your terminal to start using NVM"
   exit
-fi
+end
 
-if ! grep -qc 'nvm.sh' $PROFILE; then
+if not grep -qc 'nvm.sh' $PROFILE
   echo "=> Appending source string to $PROFILE"
-  echo $SOURCE_STR >> "$PROFILE"
+  echo $SOURCE_STR >> $PROFILE
 else
   echo "=> Source string already in $PROFILE"
-fi
+end
 
 echo "=> Close and reopen your terminal to start using NVM"
